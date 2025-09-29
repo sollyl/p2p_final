030b1da90176e433f2c8c650fa9892613aac2b10
Lyle <lylesolomons115@gmail.com>
2025-09-29 12:47:18 +0200

    saved

--- forReal/src/main/java/za/ac/cput/forreal/TimetableController.java
+++ forReal/src/main/java/za/ac/cput/forreal/TimetableController.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package za.ac.cput.forreal;
 
 import java.net.URL;
@@ -6,10 +10,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.shape.Circle;
 
-public class DashboardController implements Initializable {
+/**
+ * FXML Controller class
+ *
+ * @author lyeso
+ */
+public class TimetableController implements Initializable {
 
     @FXML
     private ImageView prof;
@@ -17,16 +24,13 @@ public class DashboardController implements Initializable {
     private Label username;
     @FXML
     private Label account_type;
-    @FXML
-    private Label display_fullName;
-    @FXML
-    private HBox holds_modules;
 
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        Circle clip = new Circle(20, 20, 20); // centerX, centerY, radius
-        prof.setClip(clip);
-    } 
+    }    
     
 }
